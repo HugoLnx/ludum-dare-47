695298f2d699016c2eeaf41af8dc0bc37dd3de76
Felipe Villela <felipevillela@poli.ufrj.br>
2020-10-03 20:31:34 -0300

    hero movement

--- Assets/Game/Scripts/Gameplay/Hero.cs
+++ Assets/Game/Scripts/Gameplay/Hero.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Hero : MonoBehaviour
 {
+    public float speed;
+
+    Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.RightArrow)){
+            transform.Translate (Vector2.right * speed);
+        }
+        if(Input.GetKey(KeyCode.LeftArrow)){
+            transform.Translate (Vector2.right * -speed);
+        }
+        if(Input.GetKey(KeyCode.DownArrow)){
+             transform.Translate (Vector2.up * -speed);
+        }
+        if(Input.GetKey(KeyCode.UpArrow)){
+            transform.Translate (Vector2.up * speed);
+        }
     }
 }
