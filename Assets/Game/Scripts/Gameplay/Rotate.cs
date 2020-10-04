6aef96aade58b6c019d6be31718a1530f80fe03c
Felipe Villela <felipevillela@poli.ufrj.br>
2020-10-04 17:55:10 -0300

    Rotação armadilha

---
+++ Assets/Game/Scripts/Gameplay/Rotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+    [SerializeField] private float speed;
+    public GameObject sprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sprite.transform.Rotate (0, 0, Time.deltaTime * speed);
+    }
+}
