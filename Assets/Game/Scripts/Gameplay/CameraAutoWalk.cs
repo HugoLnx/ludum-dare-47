0337e33290c482bc03368110030bd0aacf1dd796
hugolnx <hugolnx@gmail.com>
2020-10-04 20:29:56 -0300

    Camera auto walk

---
+++ Assets/Game/Scripts/Gameplay/CameraAutoWalk.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAutoWalk : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Camera cam;
+
+    private void Awake() {
+        this.cam = Camera.main;
+    }
+
+    private void Update() {
+        this.cam.transform.position += Vector3.right * speed * Time.deltaTime;
+    }
+}
